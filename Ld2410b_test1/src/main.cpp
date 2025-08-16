81effba5f785042b3c5663459c6ae1b619b382a2
1ConHeoUnIn <skullcaptainp@gmail.com>
2025-08-16 15:32:40 +0700

    ld2410b test 1: fail

--- Ld2410b_test1/src/main.cpp
+++ Ld2410b_test1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ld2410.h>
+
 #define MONITOR_SERIAL Serial
 #define RADAR_SERIAL Serial1
 #define RADAR_RX_PIN 20
@@ -8,38 +9,24 @@
 ld2410 radar;
 
 uint32_t lastReading = 0;
-bool radarConnected = false;
 
 void setup(void)
 {
-  MONITOR_SERIAL.begin(115200); //Feedback over Serial Monitor
-  //radar.debug(MONITOR_SERIAL); //Uncomment to show debug information from the library on the Serial Monitor. By default this does not show sensor reads as they are very frequent.
-  #if defined(ESP32)
-    RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
-  #elif defined(__AVR_ATmega32U4__)
-    RADAR_SERIAL.begin(256000); //UART for monitoring the radar
-  #endif
+   MONITOR_SERIAL.begin(115200);
+  RADAR_SERIAL.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN);
   delay(500);
-  MONITOR_SERIAL.print(F("\nConnect LD2410 radar TX to GPIO:"));
-  MONITOR_SERIAL.println(RADAR_RX_PIN);
-  MONITOR_SERIAL.print(F("Connect LD2410 radar RX to GPIO:"));
-  MONITOR_SERIAL.println(RADAR_TX_PIN);
-  MONITOR_SERIAL.print(F("LD2410 radar sensor initialising: "));
 
-  
-  if(radar.begin(RADAR_SERIAL))
-  {
-    MONITOR_SERIAL.println(F("OK"));
-    MONITOR_SERIAL.print(F("LD2410 firmware version: "));
+  MONITOR_SERIAL.println(F("LD2410 radar sensor initializing..."));
+  if (radar.begin(RADAR_SERIAL)) {
+    MONITOR_SERIAL.println(F("Radar connected."));
+    MONITOR_SERIAL.print(F("Firmware version: "));
     MONITOR_SERIAL.print(radar.firmware_major_version);
     MONITOR_SERIAL.print('.');
     MONITOR_SERIAL.print(radar.firmware_minor_version);
     MONITOR_SERIAL.print('.');
     MONITOR_SERIAL.println(radar.firmware_bugfix_version, HEX);
-  }
-  else
-  {
-    MONITOR_SERIAL.println(F("not connected"));
+  } else {
+    MONITOR_SERIAL.println(F("Radar not connected."));
   }
 }
 
