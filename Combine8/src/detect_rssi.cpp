f7c85dec18afba12256593052f35df94a71785f0
1ConHeoUnIn <skullcaptainp@gmail.com>
2025-11-12 15:43:59 +0700

    can accept

---
+++ Combine8/src/detect_rssi.cpp
@@ -0,0 +1,53 @@
+// dectect_rssi.cpp
+#include "detect_rssi.h"
+
+detect_rssi::detect_rssi()
+{
+    ssid = "pa";
+    password ="12345678";
+    rssi_filter = KalmanFilter(0.0, 1.0, 1.0, 0.95); // x, p , r, a
+}
+void detect_rssi::begin()
+{
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi");
+    int attempt = 0;
+    while (WiFi.status() != WL_CONNECTED && attempt < 20)
+    {
+        delay(500);
+        Serial.print(".");
+        attempt++;
+    }
+    Serial.println("Connected to WiFi");
+}
+
+long detect_rssi::get_rssi()
+{
+    /*
+    // filter
+    long rssi_raw = WiFi.RSSI();
+
+    
+    long rssi = rssi_filter.update(rssi_raw);
+    */
+   // none filter
+   long rssi = WiFi.RSSI();
+
+
+    Serial.print("RSSI: ");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+    return rssi;
+}
+
+void detect_rssi::stop()
+{
+    WiFi.disconnect();
+}
+const char* detect_rssi::get_ssid() {
+    return ssid;
+}
+
+const char* detect_rssi::get_password() {
+    return password;
+}
